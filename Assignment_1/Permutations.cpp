74e7fc29f98f9f50c80e3fb2a8e80b5f60050ce1
Aida Getoeva <agetoeva@213.180.217.77-red.dhcp.yndx.net>
2017-05-19 18:16:10 +0300

    First assignment. Each source file includes comments.

---
+++ Assignment_1/Permutations.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+// function which returns lowercase character for uppercase english symbol
+// for any other characters it returs the character itself
+char to_lower(char c) {
+    return c >= 'A' && c<= 'Z' ? c - ('A' - 'a') : c;
+}
+
+// This function takes references for 2 strings and return:
+// * false, if the strings are not permutations of each other
+// * true, if the strings are permutations of each other
+//
+// Notice: function isn't case sensitive for English characters.
+// It means that:
+//    Listen | Silent -> true
+//    tree | TEER -> true
+//    й13 | 1Й3 -> false
+// Compare with:
+//    й13 | 1й3 -> true
+bool CheckPermutations(const std::string& a, const std::string& b) {
+    // simple case
+    if (a.size() != b.size())
+        return false;
+
+    std::map<char, int> alph;
+    // for each character in first string we'll increase the number of repetitions of this symbol
+    // and for each character in the second string -- will decrease
+    for (int i = 0; i < a.size(); i++) {
+        // we use to_lower(...) because CheckPermutations(a, b) function isn't case-sensitive
+        // for English alphabets
+        char ai = to_lower(a[i]);
+        char bi = to_lower(b[i]);
+        alph[ai] = alph[ai] + 1;
+        alph[bi] = alph[bi] - 1;
+    }
+
+    // the idea is that if we have 2 permutations all of alph's keys will be zeros
+    // in other case there will be at least a one key which != 0
+    for (std::map<char, int>::iterator it = alph.begin(); it != alph.end(); it++)
+        if (it->second != 0)
+            return false;
+    return true;
+}
+
+int main() {
+    std::string a, b;
+    std::cin >> a >> b;
+
+    if (CheckPermutations(a, b))
+        std::cout << "Yes" << std::endl;
+    else
+        std::cout << "No" << std::endl;
+
+    return 0;
+}
