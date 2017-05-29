e68a90a615f5f232aa064b2fb6ff2707c17a975b
Aida Getoeva <agetoeva@agetoeva-osx3.local>
2017-05-29 13:17:27 +0300

    Assignment 2 with tests

--- Assignment_2/tree-inl.h
+++ Assignment_2/tree-inl.h
@@ -1,73 +1,188 @@
 #include "tree.h"
 
 #include <queue>
+#include <stdexcept>
 
 template <class T>
-TBinaryTree<T>::TBinaryTree(const T& root) {
-    Root = new TTreeNode(root);
+TBinaryTree<T>::TBinaryTree(const T& key, unsigned depth, TBinaryTree<T>* left, TBinaryTree<T>* right)
+    : Key(key)
+    , Depth(depth)
+    , Left(left)
+    , Right(right)
+{
+    if (Left)
+        Left->Parent = this;
+    if(Right)
+        Right->Parent = this;
 }
 
 template <class T>
-TBinaryTree<T>::TBinaryTree(const T& root, const TBinaryTree<T>& left, const TBinaryTree<T>& right) {
-    Root = new TTreeNode(root, left.Root, right.Root);
+TBinaryTree<T>::TBinaryTree(const T& key, TBinaryTree<T>* left, TBinaryTree<T>* right)
+    : TBinaryTree(key, 0, left, right)
+{
+    if (Left)
+        Left->ComputeDepths(1);
+    if(Right)
+        Right->ComputeDepths(1);
 }
 
 template <class T>
-TBinaryTree<T>::TBinaryTree(const std::vector<T>& values) {
-    Root = BuildTree(values, 0);
+T TBinaryTree<T>::GetKey() {
+    return Key;
 }
 
 template <class T>
-typename TBinaryTree<T>::TTreeNode* TBinaryTree<T>::BuildTree(const std::vector<T>& values, int idxRoot) {
+unsigned TBinaryTree<T>::GetDepth() {
+    return Depth;
+}
+
+template <class T>
+TBinaryTree<T>* TBinaryTree<T>::GetLeft() {
+    return Left;
+}
+
+template <class T>
+TBinaryTree<T>* TBinaryTree<T>::GetRight() {
+    return Right;
+}
+
+template <class T>
+TBinaryTree<T>* TBinaryTree<T>::BuildFullBinaryTree(const std::vector<T>& values) {
+    // Tree cannot be empty.
+    if (values.size() < 1)
+        throw std::invalid_argument("Vector should have at least one item.");
+
+    return new TBinaryTree(values[0], 0, BuildTree(values, 1, 1), BuildTree(values, 2, 1));
+}
+
+template <class T>
+TBinaryTree<T>* TBinaryTree<T>::BuildTree(const std::vector<T>& values, int idxRoot, unsigned depth) {
+    // Leaf nodes have nullptrs children.
+    if (idxRoot >= values.size())
+        return nullptr;
+
     int idxLeft = 2 * idxRoot + 1;
     int idxRight = idxLeft + 1;
-    if (idxLeft >= values.size()) {
-        return new TTreeNode(values[idxRoot]);
-    } else if (idxRight >= values.size()) {
-        return new TTreeNode(values[idxRoot], BuildTree(values, idxLeft), nullptr);
-    } else {
-        return new TTreeNode(values[idxRoot], BuildTree(values, idxLeft), BuildTree(values, idxRight));
-    }
+    return new TBinaryTree(values[idxRoot], depth, BuildTree(values, idxLeft, depth + 1), BuildTree(values, idxRight, depth + 1));
 }
 
 template <class T>
 void TBinaryTree<T>::Print(std::ostream &out) {
-    out << "Binary tree:" << std::endl;
+    if (!Parent)
+        out << "Root ";
+    else
+        out << Parent->Key << " -> ";
+    out << Key << "(" << Depth << ")" << std::endl;
 
-    std::queue<TTreeNode*> nodes;
-    nodes.push(Root);
-
-    while (!nodes.empty()) {
-        TTreeNode* node = nodes.front();
-        if (!node) {
-            break;
-        }
-
-        out << node->Key << " ";
-
-        nodes.push(node->Left);
-        nodes.push(node->Right);
-        nodes.pop();
-    }
-
-    out << std::endl;
+    if (Left)
+        Left->Print(out);
+    if (Right)
+        Right->Print(out);
 }
 
 template <class T>
 TBinaryTree<T>::~TBinaryTree() {
-    std::queue<TTreeNode*> nodes;
-    nodes.push(Root);
+    std::cout << Key << " ";
+    if (Left)
+        delete Left;
+    if(Right)
+        delete Right;
+}
 
-    while (!nodes.empty()) {
-        TTreeNode* node = nodes.front();
-        if (!node) {
-            break;
+template <class T>
+void TBinaryTree<T>::ComputeDepths(unsigned depth) {
+    Depth = depth;
+    if (Left)
+        Left->ComputeDepths(depth + 1);
+    if (Right)
+        Right->ComputeDepths(depth + 1);
+}
+
+template <class T>
+TBinaryTree<T>* TBinaryTree<T>::SetLeft(TBinaryTree<T>* left) {
+    if (Left)
+        delete Left;
+
+    Left = left;
+    Left->ComputeDepths(Depth + 1);
+
+    return this;
+}
+
+template <class T>
+TBinaryTree<T>* TBinaryTree<T>::SetRight(TBinaryTree<T>* right) {
+    if (Right)
+        delete Right;
+
+    Right = right;
+    Right->ComputeDepths(Depth + 1);
+
+    return this;
+}
+
+template <class T>
+bool TBinaryTree<T>::PrintAncestors(const T &key, std::ostream &out) {
+    // Firsly we find a node with the given tree
+    TBinaryTree* node = FindKey(key);
+    // after we just go from the node to root by Parent links
+    if (node) {
+        out << "All ancestors of the node " << key << ":";
+        node = node->Parent;
+        while (node) {
+            out << " " << node->Key;
+            node = node->Parent;
         }
+        out << std::endl;
 
-        nodes.push(node->Left);
-        nodes.push(node->Right);
-
-        nodes.pop();
-        delete node;
+        return true;
     }
+
+    return false;
+}
+
+template <class T>
+TBinaryTree<T>* TBinaryTree<T>::FindKey(const T& key) {
+    if (Key == key)
+        return this;
+
+    if (Left) {
+        TBinaryTree* inLeft = Left->FindKey(key);
+        if (inLeft)
+            return inLeft;
+    }
+
+    if (Right) {
+        TBinaryTree* inRight = Right->FindKey(key);
+        if (inRight)
+            return inRight;
+    }
+
+    return nullptr;
+}
+
+template <class T>
+TBinaryTree<T>* TBinaryTree<T>::LeastCommonAncestor(TBinaryTree<T>* first, TBinaryTree<T>* second) {
+    // Validate arguments
+    if (!first || !second)
+        throw std::invalid_argument("Expected pointers for subtrees but nullptrs were given.");
+
+    // The idea is to go from bottom to up by both first and second pointers.
+    // So firsly we should put pointers for the same depth in the tree because
+    // in another way we cannot move both of them.
+    // After we move them while they are not equal.
+    unsigned commonDepth = std::min(first->Depth, second->Depth);
+    while (first->Depth > commonDepth)
+        first = first->Parent;
+    while (second->Depth > commonDepth)
+        second = second->Parent;
+
+    while (first != second) {
+        first = first->Parent;
+        second = second->Parent;
+    }
+
+    // If they are equal to nullptr the given nodes are from different trees.
+    if (!first)
+        throw std::invalid_argument("Given nodes are from different trees.");
+    return first;
 }
--- Assignment_2/tree.h
+++ Assignment_2/tree.h
@@ -3,35 +3,81 @@
 #include <iostream>
 #include <vector>
 
+// Implementation of a binary tree data structure.
 template <class T>
 class TBinaryTree {
-    struct TTreeNode {
-        T Key;
-        TTreeNode* Left = nullptr;
-        TTreeNode* Right = nullptr;
+    T Key;
+    // Depth of the node in the BT. The root depth is 0.
+    unsigned Depth = 0;
 
-        TTreeNode(T value) : Key(value) {}
-        TTreeNode(const T& key, TTreeNode* left, TTreeNode* right)
-            : Key(key)
-            , Left(left)
-            , Right(right)
-        {}
-    };
-
-    TTreeNode* Root = nullptr;
+    TBinaryTree* Parent = nullptr;
+    TBinaryTree* Left = nullptr;
+    TBinaryTree* Right = nullptr;
 
 public:
-    TBinaryTree() {}
-    TBinaryTree(const T& root);
-    TBinaryTree(const T& root, const TBinaryTree<T>& left, const TBinaryTree<T>& right);
-    TBinaryTree(const std::vector<T>& values);
+    // Creating a root tree
+    TBinaryTree(T value) : Key(value) {}
+    // For creating a tree "from bottom to up"
+    //
+    // Notice: because of the Depth field in each tree node to maintain a consistency of all depths
+    // values, constructor calls ComputeDepths() method for the left and right subtrees to
+    // recompute depths. So this works O(n) where n is number of items in the left and right subtrees both.
+    TBinaryTree(const T& key, TBinaryTree<T>* left, TBinaryTree<T>* right);
 
     ~TBinaryTree();
 
+    // Sets left or right subtrees. If there is already subtree it will be deleted.
+    //
+    // Notice: these methods recompute depths too, so each of them works
+    // O(n) where n is number of items in the left or right subtree.
+    TBinaryTree<T>* SetLeft(TBinaryTree<T>* left);
+    TBinaryTree<T>* SetRight(TBinaryTree<T>* right);
+
+    // Just getters
+    T GetKey();
+    unsigned GetDepth();
+    TBinaryTree<T>* GetLeft();
+    TBinaryTree<T>* GetRight();
+
+    // In assumption that there are only unique keys in the tree this method returns
+    // pointer to the node with the given key. It uses DFS for searching.
+    TBinaryTree<T>* FindKey(const T& key);
+
+    // Generates full BT by given vector of keys which is string representation of a full BT.
+    // More formally for each i=1,2,..,n (where n - number of the tree nodes) node with the Key = values[i]
+    // has two children: node with Key = values[2 * i + 1] and node with Key = values[2 * i + 2].
+    //
+    // For example:
+    //      values = {0, 1, 2, 3, 4, 5}
+    // The tree will be:
+    //        0
+    //      /  \
+    //     1   2
+    //    /\  /\
+    //   3 4 5
+    static TBinaryTree* BuildFullBinaryTree(const std::vector<T>& values);
+
+    // Returns pointer for LCA of two nodes in a tree.
+    // If nullptr was given as one of the arguments methods throws argument_invalid exception.
+    // Also if two nodes don't have an ancestor (they are from different trees) it also throws
+    // argument_invalid exception.
+    static TBinaryTree* LeastCommonAncestor(TBinaryTree<T>* first, TBinaryTree<T>* second);
+
+    // Prints all ancestors of a node with the given key except itself to the given stream
+    // and returns true or false wether there is a such node in the tree.
+    bool PrintAncestors(const T& key, std::ostream& out);
+    // Prints tree.
     void Print(std::ostream& out);
 
 private:
-    TTreeNode* BuildTree(const std::vector<T>& values, int idxRoot);
+    // Helper constructor for the BuildFullBinaryTree method. It doesn't recompute depths
+    // so it works faster for another one.
+    TBinaryTree(const T& key, unsigned depth, TBinaryTree<T>* left, TBinaryTree<T>* right);
+    // Recompute depth starting from the given value.
+    void ComputeDepths(unsigned depth);
+
+    // Also helper method for BuildFullBinaryTree method.
+    static TBinaryTree<T>* BuildTree(const std::vector<T>& values, int idxRoot, unsigned depth);
 };
 
 #include "tree-inl.h"
