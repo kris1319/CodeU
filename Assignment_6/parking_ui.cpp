980ef4fe865b74ad14dac2a2b89b71fcf11b682c
julia57 <nuwanda57@yandex.ru>
2017-07-26 19:44:29 +0300

    Update parking_ui.cpp

--- Assignment_6/parking_ui.cpp
+++ Assignment_6/parking_ui.cpp
@@ -1,62 +1,50 @@
 #include "parking_ui.h"
 
-void ParkingUI::GetUsersArrangement(int NumberOfSlots, std::vector<unsigned>& arrangement) {
-    if (NumberOfSlots == 0) {
+void ParkingUI::GetUsersArrangement(int number_of_slots, std::vector<unsigned>& arrangement) {
+    if (number_of_slots == 0) {
         return;
     }
+
     std::cout << "Enter an arrangement of cars at the parking lot" << std::endl;
     unsigned car;
-    for (int i = 0; i < NumberOfSlots; ++i) {
+    for (int i = 0; i < number_of_slots; ++i) {
         std::cin >> car;
         arrangement.push_back(car);
     }
 }
 
 void ParkingUI::Run() {
-    int NumberOfSlots;
-    std::cout << "Set a number of the parking places:" << std::endl;
-    std::cin >> NumberOfSlots;
+    // variables
+    unsigned number_of_slots;
+    std::string user_answer;
 
-    if (NumberOfSlots >= 0) {
+    while (1) {
+        std::cout << "New parking is ready. Set a number of places:" << std::endl;
+        std::cin >> number_of_slots;
+
         std::vector<unsigned> arrangement;
-        GetUsersArrangement(NumberOfSlots, arrangement);
+        GetUsersArrangement(number_of_slots, arrangement);
 
         CarParking parking = CarParking(arrangement);
-        Rearrangings(NumberOfSlots, parking);
-    } else {
-        std::cout << "Number of parking places can't be less than 0" << std::endl;
-    }
+        Rearrangings(number_of_slots, parking);
 
-    std::string user_answer;
-    while (1) {
         std::cout << "Do you want to enter another arrangement (yes/no)?" << std::endl;
         std::cin >> user_answer;
+        while (user_answer != "no" && user_answer != "yes") {
+            std::cout << "Please write 'yes' or 'no'" << std::endl;
+            std::cin >> user_answer;
+        }
         if (user_answer == "no") {
             break;
-        }
-        if (user_answer != "yes") {
-            std::cout << "Please write 'yes' or 'no'" << std::endl;
-            continue;
-        }
-        std::cout << "New parking is ready. Set a number of places:" << std::endl;
-        std::cin >> NumberOfSlots;
-
-        if (NumberOfSlots >= 0) {
-            std::vector<unsigned> arrangement;
-            GetUsersArrangement(NumberOfSlots, arrangement);
-
-            CarParking parking = CarParking(arrangement);
-            Rearrangings(NumberOfSlots, parking);
-        } else {
-            std::cout << "Number of parking places can't be less than 0" << std::endl;
         }
     }
 }
 
-void ParkingUI::Rearrangings(int NumberOfSlots, CarParking& parking) {
-    if (NumberOfSlots == 0) {
+void ParkingUI::Rearrangings(int number_of_slots, CarParking& parking) {
+    if (number_of_slots == 0) {
         return;
     }
+    
     std::string user_answer;
     while (1) {
         std::cout << "Do you want to rearrange the cars (yes/no)?" << std::endl;
@@ -70,7 +58,7 @@ void ParkingUI::Rearrangings(int NumberOfSlots, CarParking& parking) {
         }
 
         std::vector<unsigned> new_arrangement;
-        GetUsersArrangement(NumberOfSlots, new_arrangement);
+        GetUsersArrangement(number_of_slots, new_arrangement);
 
         std::cout << "Rearranging process:" << std::endl;
 
